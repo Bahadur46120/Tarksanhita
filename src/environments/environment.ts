cd4117f035088c2b155fb88b973e099ac1cbd784
bahadur3028 <bahadur3028@gmail.com>
2026-08-24 16:38:37 +0530

    apiUrl

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: false,
-  apiUrl: 'http://johnkumar-001-site1.etempurl.com/api',
+  apiUrl: 'https://johnkumar-001-site1.etempurl.com/api',
   /** Built-in accounts (see core/demo-accounts.ts) sign in without the API. */
   demoLogin: true,
   siteName: 'Tarksanhita',
